57f93c5ea36519c9fdd2b1aa0eb9005b75c5c909
Sujan <156099795+whatdaahaael@users.noreply.github.com>
2024-12-13 22:52:01 +0530

    Update LinkedList.c
    Added: 
    1. Linear Search
    2. Max Number
    Fixed:
    1. Deletion Operations
    2. Use of 'free()'
    3. Syntax Mistakes

--- LinkedList.c
+++ LinkedList.c
@@ -21,11 +21,13 @@ void InitList();
 void InLL(int Num);
 void DisList(LN *Head); // {10->20->30->40...}
 void DelNum(int Num);
+int LinSea(int Num);
+int MaxNo();
 void Menu(int *Choice);
 
 main()
 {
-	int Choice, Num;
+	int Choice, Num, Max;
 	
 	InitList();
 		
@@ -49,6 +51,25 @@ main()
 				DelNum(Num);
 				break;
 			case 4:
+				printf("Enter Num:\n");
+				scanf("%d",&Num);
+				int Found=LinSea(Num);
+				if (Found==1){
+					printf("Number is found\n");
+				}else{
+					printf("Number not found\n");
+				}
+				break;
+			case 5:
+				Max=MaxNo();
+				if(Max==-1){
+					printf("List is Empty\n");
+				}else{
+					printf("%d is the greatest\n",Max);
+				}
+				break;
+			    	
+			case 6:
 				exit('1');
 			default:
 				printf("Invalid Choice.\n");
@@ -62,12 +83,14 @@ main()
 }
 
 // Initialize List
-void InitList(){
+void InitList()
+{
 	Head=NULL;
 }
 
-// Insert into List
-void InLL(int Num){
+// 1. Insert into List
+void InLL(int Num)
+{
 	LN *Prev, *Curr, *Node;
 	
 	Node=(LN*)malloc(sizeof(LN));
@@ -89,7 +112,9 @@ void InLL(int Num){
 	Prev->Next=Node;
 }
 
-void DisList(LN *Head){
+// 2. Display Contents of List
+void DisList(LN *Head)
+{
 	LN *Curr;
 	
 	if(Head==NULL){
@@ -108,9 +133,12 @@ void DisList(LN *Head){
 	printf("\n");
 }
 
-void DelNum(int Num){
+// 3. Delete a Number
+void DelNum(int Num)
+{
 	LN *Prev, *Curr;
 	
+	// List is Empty
 	if(Head==NULL){
 		printf("List Empty.\n");
 		return;
@@ -122,23 +150,78 @@ void DelNum(int Num){
         Curr=Curr->Next;
     }
     
+    // Number Does not exist
 	if(Curr==NULL){
 		printf("Number does not Exist.\n");
 		return;
 	}
 	
+	// Located at head
 	if(Prev == NULL) {
         Head = Curr->Next;
-    } else {
+    } else { // Located elsewhere
         Prev->Next = Curr->Next;
     }
     printf("Number Deleted.\n");
 }
 
-void Menu(int *Choice){
+// 4. Linear Search
+int LinSea(int Num)
+{
+	LN *Curr;
+	
+	//case 1: No list
+	if(Head==NULL){
+		return -1;
+	}
+	
+	Curr=Head;
+	while(Curr!=NULL){
+		if(Curr->Data==Num){
+			return 1;
+		}
+		Curr=Curr->Next;
+	}
+	return -1;
+}
+
+// 5. Max Number
+int MaxNo()
+{
+	LN *Curr;
+	int Max;
+	
+	// Case 1: No list
+	if(Head==NULL){
+		return -1;
+	}
+	
+	// Case 2: Only one number
+	if (Head->Next == NULL) {
+		Max=Head->Data;
+        return Max;
+    }
+    
+    // Case 3: Full List
+	Curr=Head;
+	Max=Curr->Data;
+	while(Curr!=NULL){
+		if(Max<Curr->Data){
+			Max=Curr->Data;
+		}
+		Curr=Curr->Next;
+	}
+	return Max;
+}
+
+// Menu
+void Menu(int *Choice)
+{
 	system("cls");
-	printf("Enter Choice:\n");
-	printf("1. Insert Number\n2. Display List\n3. Delete A Number\n4. Exit\n");
+	printf("\t\tLINKED LIST\n");
+	printf("Menu:\n");
+	printf("1. Insert Number\n2. Display List\n3. Delete A Number\n4. Search Number\n5. Max Number\n6. Exit\n");
+	printf("\nEnter Choice: ");
 	scanf("%d", Choice);
 	printf("\n\n\n\n\n\n");
 }
